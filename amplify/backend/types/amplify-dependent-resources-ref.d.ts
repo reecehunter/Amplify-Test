c5d174df3b5dc357cda87ebbec875a5e573b3a55
Reece Hunter <heecerunter@gmail.com>
2023-10-10 19:48:21 -0600

    Added lambda function

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "testAPI": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
   "auth": {
     "testapp94286e43": {
       "AppClientID": "string",
@@ -8,6 +15,15 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "testFunction": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
     }
   }
 }
--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import logo from "./logo.svg";
 import "@aws-amplify/ui-react/styles.css";
-import {
-  withAuthenticator,
-  Button,
-  Heading,
-  Image,
-  View,
-  Card,
-} from "@aws-amplify/ui-react";
+import { withAuthenticator, Button, Heading, Image, View, Card } from "@aws-amplify/ui-react";
+import { API } from "aws-amplify";
+import { useEffect, useState } from "react";
 
 function App({ signOut }) {
+  const [data, setData] = useState(null);
+
+  async function fetchData() {
+    API.get("testAPI", "/test", {})
+      .then(res => setData(res.success))
+      .catch(err => console.error(err));
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <View className="App">
       <Card>
-        <Image src={logo} className="App-logo" alt="logo" />
-        <Heading level={1}>We now have Auth!</Heading>
+        <Heading level={1}>{data || "loading..."}</Heading>
       </Card>
       <Button onClick={signOut}>Sign Out</Button>
     </View>
